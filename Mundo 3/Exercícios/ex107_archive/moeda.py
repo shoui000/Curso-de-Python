51000add78df6d6f21c23a95f7c6a5967caf57f8
shoui000 <dimydeveza@gmail.com>
2021-04-08 15:44:23 -0300

    ex107

---
+++ Mundo 3/Exercícios/ex107_archive/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(n, p):
+    p /= 100
+    p = 1 + p
+    return n * p
+
+def diminuir(n, p):
+    p /= 100
+    p = 1 - p
+    return n * p
+
+def dobro(n):
+    return n*2
+
+def metade(n):
+    return n/2
