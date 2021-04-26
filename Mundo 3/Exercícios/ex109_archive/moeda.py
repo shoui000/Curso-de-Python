5a6ff18048d5727e4557f2cf316de11bfbf18a8a
shoui000 <dimydeveza@gmail.com>
2021-04-26 13:45:19 -0300

    terminando o ex109

--- Mundo 3/Exercícios/ex109_archive/moeda.py
+++ Mundo 3/Exercícios/ex109_archive/moeda.py
@@ -1,20 +1,34 @@
-def aumentar(n = 0, p = 0, m=''):
+def aumentar(n = 0, p = 0, m=False):
     p /= 100
     p = 1 + p
-    if len(m) = 0:
+    if m:
+        resu = n * p
+        return moeda(p=resu)
+    else:
         return n * p
-    return n * p
 
-def diminuir(n = 0, p = 0):
+def diminuir(n = 0, p = 0, m=False):
     p /= 100
     p = 1 - p
-    return n * p
+    if m:
+        resu = n * p
+        return moeda(p=resu)
+    else:
+        return n * p
 
-def dobro(n = 0):
-    return n*2
+def dobro(n = 0, m=False):
+    if m:
+        resu = n*2
+        return moeda(p=resu)
+    else:
+        return n*2
 
-def metade(n = 0):
-    return n/2
+def metade(n = 0, m=False):
+    if m:
+        resu = n/2
+        return moeda(p=resu)
+    else:
+        return n/2
 
 def moeda(p = 0, m = 'R$'):
     return f'{m}{p:.2f}'.replace('.',',')
